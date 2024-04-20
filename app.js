adeab87619220bd73c052d52735cfe4e569bfec8
Dmitriy Grosh <40547597+DmitriyGrosh@users.noreply.github.com>
2024-04-20 14:39:02 +0300

    add routes and public file to repo

--- app.js
+++ app.js
@@ -2,21 +2,24 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const connectToDatabase = require('./database/connect');
 
-// const gamesRouter = require('./routes/games');
-// const categoriesRouter = require('./routes/categories');
-// const usersRouter = require('./routes/users');
+const gamesRouter = require('./routes/games');
+const categoriesRouter = require('./routes/categories');
+const usersRouter = require('./routes/users');
 
 const PORT = 3000;
 
 const app = express();
 
+connectToDatabase();
+
 app.use(
 	bodyParser.json(),
 	express.static(path.join(__dirname, 'public')),
-	// usersRouter, 
-  // gamesRouter, 
-  // categoriesRouter
+	usersRouter, 
+  gamesRouter, 
+  categoriesRouter
 );
 
 app.listen(PORT, () => {
